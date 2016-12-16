cebe5bf34d04a8488054aa806aa41a6b6fb57f37
Oscar Nord <oscar.nord@gmail.com>
2016-12-16 16:50:00 +0100

    reformat code

--- lab1/stubb.cpp
+++ lab1/stubb.cpp
@@ -14,40 +14,37 @@
 #include <osg/AnimationPath>
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
     osg::ref_ptr<osg::Group> root = new osg::Group;
 
 #if 1
     /// Line ---
 
-    osg::Vec3 line_p0 (-1, 0, 0);
-    osg::Vec3 line_p1 ( 1, 0, 0);
+    osg::Vec3 line_p0(-1, 0, 0);
+    osg::Vec3 line_p1(1, 0, 0);
 
     osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array();
     vertices->push_back(line_p0);
     vertices->push_back(line_p1);
 
     osg::ref_ptr<osg::Vec4Array> colors = new osg::Vec4Array;
-    colors->push_back(osg::Vec4(0.9f,0.2f,0.3f,1.0f));
+    colors->push_back(osg::Vec4(0.9f, 0.2f, 0.3f, 1.0f));
 
     osg::ref_ptr<osg::Geometry> linesGeom = new osg::Geometry();
     linesGeom->setVertexArray(vertices);
     linesGeom->setColorArray(colors, osg::Array::BIND_OVERALL);
 
-    linesGeom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES,0,2));
+    linesGeom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES, 0, 2));
 
     osg::ref_ptr<osg::Geode> lineGeode = new osg::Geode();
     lineGeode->addDrawable(linesGeom);
-    lineGeode->getOrCreateStateSet()->setMode(GL_LIGHTING,osg::StateAttribute::OFF);
+    lineGeode->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
 
     root->addChild(lineGeode);
 
     /// ---
 #endif
-
-
-    // Add your stuff to the root node here...
 
     //set dim of ground plane
     const int dimX = 256;
@@ -63,7 +60,6 @@ int main(int argc, char *argv[]){
     groundTexture->setWrap(osg::Texture::WRAP_S, osg::Texture::REPEAT);
     groundTexture->setWrap(osg::Texture::WRAP_R, osg::Texture::REPEAT);
 
-
     //define ground
     osg::ref_ptr<osg::HeightField> field = new osg::HeightField();
 
@@ -71,19 +67,18 @@ int main(int argc, char *argv[]){
     field->allocate(dimX, dimY);
     field->setXInterval(1.0f);
     field->setYInterval(1.0f);
-    field->setOrigin( osg::Vec3( -(dimX / 2), -(dimY / 2) , 0.0f) );
+    field->setOrigin(osg::Vec3(-(dimX / 2), -(dimY / 2), 0.0f));
 
     //set the Height at each point
-    for(unsigned int r = 0; r < field->getNumRows(); r++) {
+    for (unsigned int r = 0; r < field->getNumRows(); r++) {
         for (unsigned int c = 0; c < field->getNumColumns(); c++) {
 
             //std::cout << "Column: " << c << " Row: " << r << " Value: " << ( (float)*heightMap-> data( c, r ) / 255 ) <<std:: endl;
             //field->setHeight(r, c, cos(r/8) + sin(c/8));
-            float heightMapVal = ( (float)*heightMap-> data( c, r )/ 12 );
-            field->setHeight( c,r, heightMapVal);
+            float heightMapVal = ((float) *heightMap->data(c, r) / 12);
+            field->setHeight(c, r, heightMapVal);
         }
     }
-
 
     //add ground with texture to scene
     osg::ref_ptr<osg::Geode> groundGeode = new osg::Geode();
@@ -93,47 +88,36 @@ int main(int argc, char *argv[]){
     //add to root
     root->addChild(groundGeode);
 
-
     //define model
     osg::ref_ptr<osg::Node> gliderNode = osgDB::readNodeFile("cessna.osg");
     osg::ref_ptr<osg::PositionAttitudeTransform> gliderNodeTransform =
             new osg::PositionAttitudeTransform();
     gliderNodeTransform->addChild(gliderNode);
-    gliderNodeTransform->setScale(osg::Vec3(1,1,1));
-
+    gliderNodeTransform->setScale(osg::Vec3(1, 1, 1));
 
     //set animation path
     osg::ref_ptr<osg::AnimationPath> gliderPath = new osg::AnimationPath();
-
     osg::AnimationPath::ControlPoint p1(
-            osg::Vec3(50 ,50 , 50));
-
-    p1.setScale(osg::Vec3(1,1,1));
-
+            osg::Vec3(50, 50, 50));
+    p1.setScale(osg::Vec3(1, 1, 1));
     osg::AnimationPath::ControlPoint p2(
             osg::Vec3(50, 500, 50));
-
-    p2.setScale(osg::Vec3(1,1,1));
-
+    p2.setScale(osg::Vec3(1, 1, 1));
     osg::AnimationPath::ControlPoint p3(
-            osg::Vec3(50, -700 , 50));
+            osg::Vec3(50, -700, 50));
+    p3.setScale(osg::Vec3(1, 1, 1));
 
-    p3.setScale(osg::Vec3(1,1,1));
-
-
-    gliderPath->insert(0.0f,p1);
-    gliderPath->insert(2.0f,p2);
-    gliderPath->insert(4.0f,p3);
+    gliderPath->insert(0.0f, p1);
+    gliderPath->insert(2.0f, p2);
+    gliderPath->insert(4.0f, p3);
 
     gliderPath->setLoopMode(osg::AnimationPath::SWING);
     osg::ref_ptr<osg::AnimationPathCallback> glidercb =
             new osg::AnimationPathCallback(gliderPath);
     gliderNodeTransform->setUpdateCallback(glidercb);
 
-
     //add to root
     root->addChild(gliderNodeTransform);
-
 
     //Add light to scene
     osg::ref_ptr<osg::LightSource> lightSource = new osg::LightSource();
@@ -141,13 +125,11 @@ int main(int argc, char *argv[]){
     light->setLightNum(0);
     light->setPosition(osg::Vec4(45, 45, 45, 1.0));
     light->setDiffuse(osg::Vec4(1.0, 0, 0, 1.0));
-    //light->setAmbient(osg::Vec4(0.3, 0.3f, 0.3f, 1.0));
+    light->setAmbient(osg::Vec4(0.03f, 0.03f, 0.03f, 1.0));
     lightSource->setLight(light);
 
     //add to root
     root->addChild(lightSource);
-
-
 
     //Add light 2 to scene
     osg::ref_ptr<osg::LightSource> lightSource2 = new osg::LightSource();
@@ -161,7 +143,6 @@ int main(int argc, char *argv[]){
     //add to root
     root->addChild(lightSource2);
 
-
     //Optimizes the scene-graph
     //osgUtil::Optimizer optimizer;
     //optimizer.optimize(root);
@@ -172,9 +153,9 @@ int main(int argc, char *argv[]){
 
     osg::ref_ptr<osg::Camera> camera = new osg::Camera;
     camera->setProjectionMatrixAsPerspective(60.0, 1.0, 0.1, 100.0);
-    camera->setViewMatrixAsLookAt (osg::Vec3d(0.0, 0.0, 2.0),
-                                   osg::Vec3d(0.0, 0.0, 0.0),
-                                   osg::Vec3d(0.0, 1.0, 0.0));
+    camera->setViewMatrixAsLookAt(osg::Vec3d(0.0, 0.0, 2.0),
+                                  osg::Vec3d(0.0, 0.0, 0.0),
+                                  osg::Vec3d(0.0, 1.0, 0.0));
     camera->getOrCreateStateSet()->setGlobalDefaults();
     viewer.setCamera(camera);
 
