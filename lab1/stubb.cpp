fc6bb3f91cc5db93052b91a30d3b860bab5339c8
Oscar Nord <oscar.nord@gmail.com>
2016-12-26 21:39:10 +0100

    Added LOD of a dumptruck

--- lab1/stubb.cpp
+++ lab1/stubb.cpp
@@ -1,9 +1,8 @@
 #include <osg/Version>
-#include <iostream>
 #include <osg/Node>
-#include <iostream>
 #include <osgDB/ReadFile>
 #include <osg/PositionAttitudeTransform>
+#include <osg/AnimationPath>
 #include <osg/MatrixTransform>
 #include <osgViewer/Viewer>
 #include <osgUtil/Simplifier>
@@ -11,7 +10,6 @@
 #include <osg/ShapeDrawable>
 #include <osg/CopyOp>
 #include <osgUtil/IntersectVisitor>
-#include <osg/AnimationPath>
 
 
 int main(int argc, char *argv[]) {
@@ -84,7 +82,6 @@ int main(int argc, char *argv[]) {
     osg::ref_ptr<osg::Geode> groundGeode = new osg::Geode();
     groundGeode->addDrawable(new osg::ShapeDrawable(field));
     groundGeode->getOrCreateStateSet()->setTextureAttributeAndModes(0, groundTexture);
-
     //add to root
     root->addChild(groundGeode);
 
@@ -93,7 +90,8 @@ int main(int argc, char *argv[]) {
     osg::ref_ptr<osg::PositionAttitudeTransform> gliderNodeTransform =
             new osg::PositionAttitudeTransform();
     gliderNodeTransform->addChild(gliderNode);
-    gliderNodeTransform->setScale(osg::Vec3(1, 1, 1));
+    gliderNodeTransform->setScale(osg::Vec3(5, 5, 5));
+
 
     //set animation path
     osg::ref_ptr<osg::AnimationPath> gliderPath = new osg::AnimationPath();
@@ -103,21 +101,48 @@ int main(int argc, char *argv[]) {
     osg::AnimationPath::ControlPoint p2(
             osg::Vec3(50, 500, 50));
     p2.setScale(osg::Vec3(1, 1, 1));
-    osg::AnimationPath::ControlPoint p3(
-            osg::Vec3(50, -700, 50));
-    p3.setScale(osg::Vec3(1, 1, 1));
 
     gliderPath->insert(0.0f, p1);
-    gliderPath->insert(2.0f, p2);
-    gliderPath->insert(4.0f, p3);
+    gliderPath->insert(3.0f, p2);
 
     gliderPath->setLoopMode(osg::AnimationPath::SWING);
     osg::ref_ptr<osg::AnimationPathCallback> glidercb =
             new osg::AnimationPathCallback(gliderPath);
     gliderNodeTransform->setUpdateCallback(glidercb);
-
     //add to root
     root->addChild(gliderNodeTransform);
+
+    //create dupTruck with LOD
+    osg::ref_ptr<osg::Node> dumpTruck = osgDB::readNodeFile("dumptruck.osg");
+
+    //use LODs
+    osgUtil::Simplifier simple(0.53);
+    simple.setMaximumLength(2.0f);
+
+    osg::ref_ptr<osg::Node> dumpTruckLower =
+            dynamic_cast<osg::Node*>(dumpTruck->clone(osg::CopyOp::DEEP_COPY_ALL));
+    dumpTruckLower->accept(simple);
+
+    osg::ref_ptr<osg::Node> dumpTruckLowest =
+            dynamic_cast<osg::Node*>(dumpTruck->clone(osg::CopyOp::DEEP_COPY_ALL));
+    simple.setSampleRatio(0.1f);
+    dumpTruckLowest->accept(simple);
+
+    osg::ref_ptr<osg::LOD> dumpTruckLOD = new osg::LOD();
+    dumpTruckLOD->setRangeMode( osg::LOD::DISTANCE_FROM_EYE_POINT );
+    dumpTruckLOD->addChild(dumpTruck, 0,50);
+    dumpTruckLOD->addChild(dumpTruckLower, 51,60);
+    dumpTruckLOD->addChild(dumpTruckLowest,61,10000);
+
+    osg::ref_ptr<osg::PositionAttitudeTransform> dumpTruckTransform =
+            new osg::PositionAttitudeTransform();
+
+    dumpTruckTransform->addChild(dumpTruckLOD);
+    dumpTruckTransform->setPosition(osg::Vec3(40,1,20));
+    dumpTruckTransform->setScale(osg::Vec3(1.5,1.5,1.5));
+    //add to root
+    root->addChild(dumpTruckTransform);
+
 
     //Add light to scene
     osg::ref_ptr<osg::LightSource> lightSource = new osg::LightSource();
@@ -127,7 +152,6 @@ int main(int argc, char *argv[]) {
     light->setDiffuse(osg::Vec4(1.0, 0, 0, 1.0));
     light->setAmbient(osg::Vec4(0.03f, 0.03f, 0.03f, 1.0));
     lightSource->setLight(light);
-
     //add to root
     root->addChild(lightSource);
 
@@ -139,13 +163,12 @@ int main(int argc, char *argv[]) {
     light2->setDiffuse(osg::Vec4(1.0, 0.9f, 0.9f, 1.0));
     //light2->setAmbient(osg::Vec4(0.3, 0.3f, 0.3f, 1.0));
     lightSource2->setLight(light2);
-
     //add to root
     root->addChild(lightSource2);
-
+    
     //Optimizes the scene-graph
-    //osgUtil::Optimizer optimizer;
-    //optimizer.optimize(root);
+    osgUtil::Optimizer optimizer;
+    optimizer.optimize(root);
 
     // Set up the viewer and add the scene-graph root
     osgViewer::Viewer viewer;
